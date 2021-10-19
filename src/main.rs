6c133fdc823820d8729f333ddfc927ca24a9d4ad
Subsegment <304741833@qq.com>
2021-10-19 17:05:26 +0800

    ADD a Test

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,35 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
-
-mod vga_buffer;
+#![feature(custom_test_frameworks)]
+#![test_runner(os_simple::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use os_simple::println;
 
-
-#[no_mangle] // don't mangle the name of this function
+#[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
-
     println!("Hello World{}", "!");
-    panic!("Some panic message");
+
+    #[cfg(test)]
+    test_main();
     loop {}
 }
 
+
+
 /// This function is called on panic.
+
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+///panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    os_simple::test_panic_handler(info);
 }
