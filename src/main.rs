2b4f90360c97a514fdf8f100c39a91dc81d23518
Subsegment <304741833@qq.com>
2021-10-24 20:53:40 +0800

    Paging impl

--- src/main.rs
+++ src/main.rs
@@ -6,27 +6,32 @@
 
 use core::panic::PanicInfo;
 use os_simple::println;
+use bootloader::{BootInfo, entry_point};
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> !  {
+    use os_simple::memory;
+    use x86_64::{structures::paging::Page, VirtAddr};
+    use os_simple::memory::BootInfoFrameAllocator;
+
+
+
     println!("Hello World{}", "!");
-
     os_simple::init();
 
-    use x86_64::registers::control::Cr3;
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = memory::EmptyFrameAllocator;
 
-    let (level_4_page_table, _) = Cr3::read();
-    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
+    // map an unused page
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
-    let ptr = 0x2031b2 as *mut u32;
-
-// read from a code page
-    unsafe { let x = *ptr; }
-    println!("read worked");
-
-// write to a code page
-    unsafe { *ptr = 42; }
-    println!("write worked");
+    // write the string `New!` to the screen through the new mapping
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
 
     #[cfg(test)]
     test_main();
