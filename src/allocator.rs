1276edc36b89061814fba9a3a7adfdb566717551
Subsegment <304741833@qq.com>
2021-10-31 18:29:21 +0800

    allocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,8 +1,19 @@
 use alloc::alloc::{GlobalAlloc, Layout};
 use core::ptr::null_mut;
 use linked_list_allocator::LockedHeap;
+use linked_list::LinkedListAllocator;
+use fixed_size_block::FixedSizeBlockAllocator;
+use bump::BumpAllocator;
+
+pub mod bump;
+pub mod linked_list;
+pub mod fixed_size_block;
 
 pub struct Dummy;
+
+pub const HEAP_START: usize = 0x_4444_4444_0000;
+pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
+
 
 unsafe impl GlobalAlloc for Dummy {
     unsafe fn alloc(&self, _layout: Layout) -> *mut u8 {
@@ -15,10 +26,9 @@ unsafe impl GlobalAlloc for Dummy {
 }
 
 #[global_allocator]
-static  ALLOCATOR: LockedHeap = LockedHeap::empty();
+static ALLOCATOR: Locked<FixedSizeBlockAllocator> = Locked::new(FixedSizeBlockAllocator::new());
 
-pub const HEAP_START: usize = 0x_4444_4444_0000;
-pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
+
 
 use x86_64::{
     structures::paging::{
@@ -55,4 +65,33 @@ pub fn init_heap(
 
 
     Ok(())
+}
+
+pub struct Locked<A> {
+    inner: spin::Mutex<A>,
+}
+
+impl<A> Locked<A> {
+    pub const fn new(inner: A) -> Self {
+        Locked {
+            inner: spin::Mutex::new(inner),
+        }
+    }
+
+    pub fn lock(&self) -> spin::MutexGuard<A> {
+        self.inner.lock()
+    }
+}
+
+// fn align_up(addr: usize, align: usize) -> usize {
+//     let remainder = addr % align;
+//     if remainder == 0 {
+//         addr // addr already aligned
+//     } else {
+//         addr - remainder + align
+//     }
+// }
+
+fn align_up(addr: usize, align: usize) -> usize {
+    (addr + align - 1) & !(align - 1)
 }
